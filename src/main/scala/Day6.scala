16eb6d32e1b5acc941dded2ff963fa3258bd618e
levi <velocorner.com@gmail.com>
2020-12-06 10:24:36 +0100

    day 6 - simplify

--- src/main/scala/Day6.scala
+++ src/main/scala/Day6.scala
@@ -3,15 +3,14 @@ import scala.io.Source
 object Day6 extends App {
 
   val input = Source
-    .fromResource("input6.txt")
+    .fromResource("input6_1.txt")
     .getLines()
     .map(_.trim)
     .toList
 
   val groups = input.foldLeft(List.empty[List[Set[Char]]])((accu, line) => accu match {
+    case _ if line.isEmpty => Nil +: accu
     case Nil => List(List(line.toSet))
-    case _ if line.isEmpty => List(Set.empty[Char]) +: accu
-    case head :: rest if head.forall(_.isEmpty) => List(line.toSet) +: rest
     case head :: rest => (head :+ line.toSet) +: rest
   })
 
