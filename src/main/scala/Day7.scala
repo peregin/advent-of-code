8cf85b751cf56b2be6be0df6ad19165e1e29b08d
Levente Fall <levente.fall@gmail.com>
2020-12-07 18:53:26 +0100

    day 7

---
+++ src/main/scala/Day7.scala
@@ -0,0 +1,60 @@
+import scala.io.Source
+
+/**
+  * dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+  * faded blue bags contain no other bags.
+  */
+object Day7 extends App {
+
+  val input = Source
+    .fromResource("input7.txt")
+    .getLines()
+    .map(_.trim)
+    .toList
+
+  val myShinyGold = "shiny gold"
+
+  case class Node(color: String, count: Int)
+
+  // key = bag color, value is a list of bags
+  // ugly parsing, probably could be optimized
+  val parents: Map[String, List[Node]] = input.map { line =>
+    val items       = line.split("contain")
+    val parentColor = items(0).trim.stripSuffix("bags").stripSuffix("bag").trim
+    val children = items(1).stripSuffix(".").trim match {
+      case "no other bags" => List.empty
+      case bags =>
+        bags
+          .split(',')
+          .map(_.stripSuffix("bags").stripSuffix("bag").trim)
+          .map { bag =>
+            val count = bag.takeWhile(_ != ' ')
+            val color = bag.stripPrefix(count).trim
+            Node(color, count.toInt)
+          }
+          .toList
+    }
+    (parentColor, children)
+  }.toMap
+
+  def hasBagWith(color: String, target: String): Boolean = {
+    //println(s"looking into $color")
+    val children = parents(color)
+    val colors   = children.map(_.color)
+    colors.exists(_ == target) || colors.exists(hasBagWith(_, target))
+  }
+
+  val solution1 = parents.keys.toList.map(hasBagWith(_, myShinyGold)).count(_ == true)
+  println(s"solution1=$solution1")
+
+  def countBagsFor(color: String): Int = {
+    val children = parents(color)
+    children.map { node =>
+      //println(s"looking into $node")
+      node.count * countBagsFor(node.color) + node.count
+    }.sum
+  }
+
+  val solution2 = countBagsFor(myShinyGold)
+  println(s"solution2=$solution2")
+}
