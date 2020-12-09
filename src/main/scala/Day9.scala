2a69ac6c4f7a3c4ff5983bb69c9f34f8662f32f2
Levente Fall <levente.fall@gmail.com>
2020-12-09 10:14:50 +0100

    day 9

---
+++ src/main/scala/Day9.scala
@@ -0,0 +1,31 @@
+import scala.io.Source
+
+object Day9 extends App {
+
+  val preamble = 25
+
+  val input = Source
+    .fromResource("input9.txt")
+    .getLines()
+    .map(_.trim)
+    .map(_.toLong)
+    .toList
+
+  val solution1 = input
+    .sliding(preamble + 1)
+    .find { list =>
+      val check  = list.last
+      val window = list.dropRight(1)
+      !window.combinations(2).exists(_.sum == check)
+    }
+    .get
+    .last
+  println(s"solution1=$solution1")
+
+  val indices = for (
+    i <- 0 to input.size;
+    j <- i + 1 to input.size
+  ) yield (i, j)
+  val solution2 = indices.map{case (min, max) => input.slice(min, max)}.find(_.sum == solution1).map(l => l.min + l.max).get
+  println(s"solution2=$solution2")
+}
