0191d125534794776cb5a35de20831a853902dcb
levi <velocorner.com@gmail.com>
2020-12-23 10:10:43 +0100

    day 21 part1

--- src/main/scala/Day21.scala
+++ src/main/scala/Day21.scala
@@ -1,24 +1,26 @@
-
-
 /**
-mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
-trh fvjkl sbzzf mxmxvkd (contains dairy)
-sqjhc fvjkl (contains soy)
-sqjhc mxmxvkd sbzzf (contains fish)
- */
+  * mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+  * trh fvjkl sbzzf mxmxvkd (contains dairy)
+  * sqjhc fvjkl (contains soy)
+  * sqjhc mxmxvkd sbzzf (contains fish)
+  */
 object Day21 extends Aso("input21.txt", identity) {
 
+  case class Food(ingredients: Set[String], allergens: Set[String])
 
-  val pattern = """(.*) \(contains (.*)\)""".r
-
-  case class Food(ingredients: Set[String], allergens: Set[String]) {
-    lazy val allergen2Ingredient = allergens.map(a => a -> ingredients).groupMapReduce(_._1)(_._2)(_ intersect _)
+  val entries = input.map {
+    case s"$ingredients (contains $allergens)" =>
+      Food(ingredients.split(' ').map(_.trim).toSet, allergens.split(',').map(_.trim).toSet)
   }
 
-  val entries = input.map{
-    case pattern(ingredients, allergens) => Food(ingredients.split(' ').toSet, allergens.split(' ').toSet)
-  }
+  val allergenInMultipleIngredients =
+    entries
+      .map(e => e.allergens.map(a => a -> e.ingredients))
+      .flatten
+      .groupMapReduce(_._1)(_._2)(_ intersect _)
+  val everyIngredient        = entries.flatMap(_.ingredients).toSet
+  val notAllergenIngredients = everyIngredient -- allergenInMultipleIngredients.values.flatten.toSet
+  entries.map(f => (notAllergenIngredients intersect f.ingredients).size).sum
 
-  val solution1 = ???
-  println(s"solution1=$solution1")
+  val solution1 = entries.map(f => (notAllergenIngredients intersect f.ingredients).size).sum
 }
