eed1469142e743c7b83b52aeb2c56dd4b8d2dad7
Levente Fall <levente.fall@tundra.com>
2021-01-14 23:38:45 +0100

    week1

--- src/main/scala/puzzles/Week1.scala
+++ src/main/scala/puzzles/Week1.scala
@@ -4,12 +4,18 @@ package puzzles
 object Week1 extends App {
 
   // YES or NO
+  // all values should be the same, or only one different with +1
   def isValid(s: String): String = {
-    val histo = s.groupBy(c => c).mapValues(_.size)
-    // all values should be the same, or only one different with +1
-    "YES"
+    val histogram = s.groupMapReduce(identity)(_ => 1)(_ + _).values.toList.sorted
+    histogram match {
+      case Nil                                                                                     => "YES" // specs?
+      case _ :: Nil                                                                                => "YES"
+      case list :+ last if list.distinct.size == 1 && (list.head == last || list.head == last - 1) => "YES"
+      case first :: list if list.distinct.size == 1 && (list.head == first || first == 1)          => "YES"
+      case _                                                                                       => "NO"
+    }
   }
 
-  val test = "abc"
+  val test = "abcdefghhgfedecba" // or "abcc" or "aabbc"
   println(s"$test is ${isValid(test)}")
 }
