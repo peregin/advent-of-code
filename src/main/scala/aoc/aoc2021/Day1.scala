192397c4b2f146499b4bccfbf4579888d8943d9c
levi <velocorner.com@gmail.com>
2021-12-03 22:34:30 +0100

    day1 - 2

--- src/main/scala/aoc/aoc2021/Day1.scala
+++ src/main/scala/aoc/aoc2021/Day1.scala
@@ -7,14 +7,17 @@ import scala.annotation.tailrec
 
 object Day1 extends Aoc("aoc2021/input1.txt", _.toInt):
 
-  def count(prev: Int, list: List[Int]): Int = {
+  def count(prev: Int, list: List[Int]): Int =
     list match {
       case Nil => 0
       case x :: xs =>
         val res = if (x > prev) 1 else 0
         res + count(x, xs)
     }
-  }
 
-  val res = count(Int.MaxValue, input)
-  println(s"res=$res")
+  val res1 = count(Int.MaxValue, input)
+  println(s"res1=$res1")
+
+  val input2 = input.sliding(3).map(_.sum).toList
+  val res2 = count(Int.MaxValue, input2)
+  println(s"res2=$res2")
