b5c1bc8d4e0fa18f87146efed69e99ac7b494b87
levi <velocorner.com@gmail.com>
2021-12-04 11:13:15 +0100

    day3 - 2

--- src/main/scala/aoc/aoc2021/Day3.scala
+++ src/main/scala/aoc/aoc2021/Day3.scala
@@ -7,8 +7,12 @@ import scala.annotation.tailrec
 
 object Day3 extends Aoc("aoc2021/input3.txt", identity):
 
+  extension (s: String)
+    def binaryToInt(): Int = Integer.parseInt(s, 2)
+
   val n2 = input.size / 2
-  val zeros = List.fill(input.head.size)(0)
+  val digits = input.head.size
+  val zeros = List.fill(digits)(0)
   val counters = input.foldLeft(zeros)((accu, line) => {
     accu.zipWithIndex.map {
       case (c, i) if line(i) == '1' => c + 1
@@ -16,7 +20,27 @@ object Day3 extends Aoc("aoc2021/input3.txt", identity):
     }}
   )
   val binary = counters.map(c => if (c > n2) 1 else 0)
-  val gamma = Integer.parseInt(binary.mkString, 2)
-  val epsilon = Integer.parseInt(binary.map(b => if (b == 1) 0 else 1).mkString, 2)
-  val res = gamma * epsilon
-  println(s"res=$res")
+  val gamma = binary.mkString.binaryToInt()
+  val epsilon = binary.map(b => if (b == 1) 0 else 1).mkString.binaryToInt()
+  val res1 = gamma * epsilon
+  println(s"res1=$res1")
+
+  def greaterOrEqual(self:Int, that: Int): Boolean = self >= that
+  def lessOrEqual(self: Int, that: Int): Boolean = self <= that
+
+  def filter(ix: Int, c: Char, list: List[String], check: (Int, Int) => Boolean): List[String] = {
+    val filtered = list.filter(_.charAt(ix) == c)
+    if (check(filtered.size, list.size - filtered.size)) filtered else list diff filtered
+  }
+
+  @tailrec
+  def find(ix: Int, c: Char, list: List[String], check: (Int, Int) => Boolean): String = {
+    //println(s"list=$list")
+    if ( ix >= digits || list.size <= 1) list.head
+    else find(ix + 1, c, filter(ix, c, list, check), check)
+  }
+
+  val oxygen = find(0, '1', input, greaterOrEqual)
+  val co2 = find(0, '0', input, lessOrEqual)
+  val res2 = oxygen.binaryToInt() * co2.binaryToInt()
+  println(s"res2=$res2")
