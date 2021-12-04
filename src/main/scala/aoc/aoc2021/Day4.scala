a67cbd8b6803ed1a5d3188b81de18d5745b5fa50
levi <velocorner.com@gmail.com>
2021-12-04 15:18:20 +0100

    day4 - 1

--- src/main/scala/aoc/aoc2021/Day4.scala
+++ src/main/scala/aoc/aoc2021/Day4.scala
@@ -6,14 +6,34 @@ import scala.annotation.tailrec
 
 object Day4 extends Aoc("aoc2021/input4.txt", identity):
 
+  case class Tile(number: Int, var marked: Boolean)
+  case class Board(lines: List[String]):
+    val grid: Array[Array[Tile]] = lines.map{ line =>
+      line.split(' ').filter(_.trim.nonEmpty).map(n => Tile(n.toInt, false))
+    }.toArray
+    def mark(n: Int): Unit = {
+      for (i <- 0 until grid.length; j <- 0 until grid(i).length) {
+        if (grid(i)(j).number == n) grid(i)(j).marked = true
+      }
+    }
+    def solved(): Boolean = grid.exists(_.forall(_.marked)) || grid.transpose.exists(_.forall(_.marked))
+    def sum(): Int = grid.flatten.filterNot(_.marked).map(_.number).sum
+
   // first group is the drawn numbers
   // the rest of groups represents a board
   val groups = splitByEmptyLine(input)
-  val drawn = groups.head
+  val drawn = groups.head.head.split(',').map(_.toInt).toList
+  val boards = groups.tail.map(Board)
 
-  case class Tile(number: Int, marked: Boolean)
+  def find(in: List[Int]): (Int, Board) = {
+    val n = in.head
+    boards.foreach(_.mark(n))
+    boards.find(_.solved()) match {
+      case Some(bingo) => (n, bingo)
+      case None => find(in.tail)
+    }
+  }
 
-  case class Board(lines: List[String]):
-    val grid: Array[Array[Tile]] = lines.map(_.split(' ').map(n => Tile(n.toInt, false))).toArray
-
-  println("res")
+  val (n, bingo) = find(drawn)
+  val res = n * bingo.sum()
+  println(s"res=$res")
