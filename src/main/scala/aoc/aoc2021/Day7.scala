6cc83aaa86ef900ce6532928c94eb2abae56f0d5
Levente Fall <levente.fall@tundra.com>
2021-12-07 13:16:59 +0100

    day 7 - 2

--- src/main/scala/aoc/aoc2021/Day7.scala
+++ src/main/scala/aoc/aoc2021/Day7.scala
@@ -8,7 +8,15 @@ object Day7 extends Aoc("aoc2021/input7.txt", identity):
 
   val scrabs = input.head.split(',').map(_.toInt).sorted
   val median = scrabs(scrabs.length / 2)
-  val res1 = scrabs.map(s => (s - median).abs).sum
+
+  def fuel(ref: Int, calc: (Int, Int) => Int) = scrabs.map(s => calc(s, ref)).sum
+  val res1 = fuel(median, (ref: Int, on: Int) => (ref - on).abs)
   println(s"res1=$res1")
+
+  val res2 = scrabs.map(s => fuel(s, (ref: Int, on: Int) => {
+    val delta = (ref - on).abs
+    (delta * (delta + 1)) / 2 // series 1,2,..,n,n+1
+  })).min
+  println(s"res2=$res2")
 
 
