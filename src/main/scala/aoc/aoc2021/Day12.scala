47422485a2c7b0f63604da60fbb9ac307e42ced3
levi <velocorner.com@gmail.com>
2021-12-12 12:01:26 +0100

    day 12 - 2

--- src/main/scala/aoc/aoc2021/Day12.scala
+++ src/main/scala/aoc/aoc2021/Day12.scala
@@ -12,21 +12,33 @@ object Day12 extends Aoc("aoc2021/input12.txt", identity):
 
   def isSmall(cave: String): Boolean = cave.forall(_.isLower)
 
-  def count(from: String, visited: Set[String], path: List[(String, String)]): Int = {
+  // twice once! a small cave
+  def count(from: String, path: List[String], twiceOk: Boolean): Int = {
     if from == "end" then {
-      //println(s"path: ${path.reverse}")
+      //println(s"path: ${path.mkString(",")}")
       1
     }
     else {
       val candidates = dag(from).filter{
+        // start and end can be visited only once
+        case cave if cave == "start" => false
         // small cave can't be visited again
-        case cave if isSmall(cave) => !visited.contains(cave)
+        case cave if !twiceOk && isSmall(cave) => !path.contains(cave)
+        // a single small cave can be visited twice
+        case cave if twiceOk && isSmall(cave) =>
+          val smalls = path.filter(isSmall)
+          smalls.size - smalls.distinct.size <= 1
         // large case is ok but the same edge can't be visited twice
-        case large => !path.contains((from, large))
+        case large => true
       }
-      candidates.map(next => count(next, visited + from, (from, next) :: path)).sum
+      candidates.map(next =>
+        count(next, path :+ next, twiceOk)
+      ).sum
     }
   }
 
-  val res1 = count("start", Set.empty, List.empty)
+  val res1 = count("start", List.empty, twiceOk = false)
   println(s"res1=$res1")
+
+  val res2 = count("start", List("start"), twiceOk = true)
+  println(s"res2=$res2")
