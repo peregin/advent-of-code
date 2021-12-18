c396dd350283c7dbc6de081e35310e048c5d2083
levi <velocorner.com@gmail.com>
2021-12-18 12:45:58 +0100

    day 17 - 2

--- src/main/scala/aoc/aoc2021/Day17.scala
+++ src/main/scala/aoc/aoc2021/Day17.scala
@@ -19,34 +19,32 @@ object Day17 extends Aoc("aoc2021/input17.txt", identity):
     val maxY: Int = corner1.y max corner2.y
     def contains(c: Coord): Boolean = minY <= c.y && c.y <= corner2.y && minX <= c.x && c.x <= corner2.x
 
-  def find(s: String): Option[Int] =
+  def findY(s: String): Seq[Int] =
     println(s"target area: $s")
     s match {
     case s"target area: x=$x1..$x2, y=$y1..$y2" =>
       val area = Rectangle(
         corner1 = Coord(y = y1.toInt, x = x1.toInt),
         corner2 = Coord(y = y2.toInt, x = x2.toInt))
-      find(area)
-      //simulateMaxY((0, 0), (2, 7), area)
-    case _ => None
+      trialsForY(area)
+    case _ => Nil
   }
 
-  def find(area: Rectangle): Option[Int] =
-    val n = 200
-    val trials = for {
+  def trialsForY(area: Rectangle): Seq[Int] =
+    val n = 300
+    for {
       y <- -n to n
       x <- -n to n
-    } yield Coord(y = y, x = x)
-    val results = trials.map(t => simulateMaxY(Coord.zero, t, area))
-    results.flatten.maxOption
+      s <- simulateMaxY(Coord.zero, Coord(y = y, x = x), area)
+    } yield s
 
   // max y as return if was hitting the target area
   def simulateMaxY(start: Coord, velocity: Coord, target: Rectangle): Option[Int] =
     @tailrec
     def step(from: Coord, velocity: Coord, maxY: Int): Option[Int] =
       //println(s"$from, $velocity, $maxY")
-      if (target.contains(from)) Some(maxY)
-      else if (target.minY > from.y || target.maxX < from.x) None
+      if target.contains(from) then Some(maxY)
+      else if target.minY > from.y || target.maxX < from.x then None
       else
         val newStart = from + velocity
         val newVelocity = velocity + Coord(y = -1, x = -Integer.signum(velocity.x))
@@ -55,9 +53,13 @@ object Day17 extends Aoc("aoc2021/input17.txt", identity):
 
 
   //val res1 = find("target area: x=20..30, y=-10..-5")
+  val res = findY(input.head)
   // 8911
-  val res1 = find(input.head)
+  val res1 = res.max
   println(s"res1=$res1")
+  // 4748
+  val res2 = res.size
+  println(s"res2=$res2")
 
 
 
