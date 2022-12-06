757ae526fdca807aecf06b8134cfaa558d0d384f
Levente Fall <levente.fall@gmail.com>
2022-12-06 07:55:32 +0100

    refactor

--- src/main/scala/aoc/aoc2022/Day6.scala
+++ src/main/scala/aoc/aoc2022/Day6.scala
@@ -5,8 +5,7 @@ import aoc.Aoc
 object Day6 extends aoc.Aoc("aoc2022/input6.txt", identity):
 
   val message = input(1) // 0 is the example, 1 is the real input
-  def marker(s: String, window: Int): Option[(String, Int)] =
-    s.sliding(window).zipWithIndex.find(_._1.toSet.size == window).map((s, ix) => (s, ix + window))
+  def marker(s: String, window: Int) = s.sliding(window).zip(List.tabulate(s.size)(_ + window)).find(_._1.toSet.size == window)
 
   println(s"res1: ${marker(message, 4)}")
   println(s"res2: ${marker(message, 14)}")
