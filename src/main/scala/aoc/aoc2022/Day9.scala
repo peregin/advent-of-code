2db30ecd56865e09d42981abbb9dad2552d383fd
Levente Fall <levente.fall@gmail.com>
2022-12-09 08:47:26 +0100

    day 9 - part 2

--- src/main/scala/aoc/aoc2022/Day9.scala
+++ src/main/scala/aoc/aoc2022/Day9.scala
@@ -3,23 +3,37 @@ package aoc.aoc2022
 import aoc.Aoc
 import math._
 
-object Day9 extends aoc.Aoc("aoc2022/input9sample2.txt", identity):
+object Day9 extends aoc.Aoc("aoc2022/input9.txt", identity):
 
   case class Coord(y: Int, x: Int):
     def nextHead(c: Coord): Coord = Coord(this.y + c.y, this.x + c.x)
 
-    def nextTail(h: Coord): Coord =
-      if abs(this.x - h.x) > 1 then Coord(h.y, this.x - signum(this.x - h.x))
-      else if abs(this.y - h.y) > 1 then Coord(this.y - signum(this.y - h.y), h.x)
+    def nextTail(h: Coord): Coord = {
+      val diffX = abs(this.x - h.x)
+      val diffY = abs(this.y - h.y)
+      if diffX > 1 && diffY > 1 then Coord(this.y - signum(this.y - h.y), this.x - signum(this.x - h.x))
+      else if diffX > 1 then Coord(h.y, this.x - signum(this.x - h.x))
+      else if diffY > 1 then Coord(this.y - signum(this.y - h.y), h.x)
       else this
+    }
 
   val zero = Coord(0, 0)
-  case class Rope(head: Coord, tail: Coord):
+  case class Rope(head: Coord, tail: List[Coord]):
     def next(c: Coord): Rope = {
       val nextHead = head.nextHead(c)
-      val nextTail = tail.nextTail(nextHead)
+      val nextTail = buildTail(nextHead, tail, Nil)
       Rope(nextHead, nextTail)
     }
+
+    @annotation.tailrec
+    private def buildTail(h: Coord, rest: List[Coord], accu: List[Coord]): List[Coord] =
+      if rest.isEmpty then accu else {
+        val cur = rest.head
+        val next = cur.nextTail(h)
+        buildTail(next, rest.tail, accu :+ next)
+      }
+
+    def coords(): Set[Coord] = (head +: tail).toSet
 
   val moves = input.flatMap {
     case s"L $m" => List.fill(m.toInt)(Coord(0, -1))
@@ -27,7 +41,7 @@ object Day9 extends aoc.Aoc("aoc2022/input9sample2.txt", identity):
     case s"U $m" => List.fill(m.toInt)(Coord(1, 0))
     case s"D $m" => List.fill(m.toInt)(Coord(-1, 0))
   }
-  val rope = Rope(zero, zero)
+  val rope = Rope(zero, List.fill(9)(zero))
 
   @annotation.tailrec
   def move(moves: List[Coord], rope: Rope, accu: List[Rope]): List[Rope] = {
@@ -38,16 +52,23 @@ object Day9 extends aoc.Aoc("aoc2022/input9sample2.txt", identity):
     }
   }
   val all = move(moves, rope, Nil)
-  //all.foreach(println)
 
-  val res1 = all.map(_.tail).distinct.size
-  println(s"res1: $res1") // 6269
+  //val res1 = all.map(_.tail).distinct.size
+  //println(s"res1: $res1") // 6269
 
-  case class Rope2(head: Coord, tail: List[Coord])
-  val rope2 = Rope2(zero, List.fill(9)(zero))
+  def debug(points: Set[Coord], nx: Int, ny: Int): Unit = {
+    for (cy <- ny to -ny by -1) {
+      for (cx <- -nx to nx) {
+        val c = Coord(cy, cx)
+        print(if points.contains(c) then "#" else ".")
+      }
+      println()
+    }
+  }
+  debug(all.map(_.tail.last).toSet, 15, 15)
 
-  val res2 = rope2
-  println(s"res2: $res2")
+  val res2 = all.map(_.tail.last).toSet.size
+  println(s"res2: $res2") // 2557
 
 
 
