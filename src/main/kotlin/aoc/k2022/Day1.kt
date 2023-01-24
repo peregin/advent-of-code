04588567b0ddcdaade41f17a70f4106e6b904cdf
levi <velocorner.com@gmail.com>
2023-01-24 09:51:37 +0100

    use non null assert

--- src/main/kotlin/aoc/k2022/Day1.kt
+++ src/main/kotlin/aoc/k2022/Day1.kt
@@ -1,9 +1,9 @@
 package aoc.k2022
 
 fun main() {
-    val lines = object{}.javaClass.getResource("/aoc2022/input1.txt")?.readText()
-    val groups = lines?.split("\n\n")?.map { it.lines() }
-    val calories = groups?.map { lines -> lines.sumBy { it.toInt() } }?.sortedDescending()
-    println("res1: ${calories?.max()}")
-    println("res2: ${calories?.take(3)?.sum()}")
+    val lines = object{}.javaClass.getResource("/aoc2022/input1.txt")!!.readText()
+    val groups = lines.split("\n\n").map { it.lines() }
+    val calories = groups.map { lines -> lines.sumBy { it.toInt() } }.sortedDescending()
+    println("res1: ${calories.max()}")
+    println("res2: ${calories.take(3).sum()}")
 }
