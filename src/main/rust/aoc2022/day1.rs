5e69243ed60610cfaa9dd4d920093d550496739e
levi <velocorner.com@gmail.com>
2023-07-02 21:59:03 +0200

    day 1 rust

--- src/main/rust/aoc2022/day1.rs
+++ src/main/rust/aoc2022/day1.rs
@@ -1,5 +1,18 @@
 
 
 fn main() {
-    println!("day1");
+    let input = include_str!("../../resources/aoc2022/input1.txt");
+    let back: Vec<&str> = input.lines().collect();
+    let mut caps: Vec<i32> = back
+        .split(|s| s.is_empty())
+        .map(|s| s.iter().map(|i| i.trim().parse::<i32>().unwrap()).sum())
+        .collect();
+    println!("caps {:?}", caps);
+
+    let res1 = caps.iter().max().unwrap();
+    println!("res1 {}", res1);
+
+    caps.sort();
+    let res2: i32 = caps.iter().rev().take(3).sum();
+    println!("res2 {:?}", res2);
 }
