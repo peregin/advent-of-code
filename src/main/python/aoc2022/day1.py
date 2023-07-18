778ff7bbc1f9cfd2424d8664e109596a29158e2f
levi <velocorner.com@gmail.com>
2023-07-18 23:00:11 +0200

    day 1 python

--- src/main/python/aoc2022/day1.py
+++ src/main/python/aoc2022/day1.py
@@ -1,6 +1,13 @@
-
 def sol():
-    print("Part 1:")
+    # read file
+    with open("../../resources/aoc2022/input1.txt") as f:
+        elves = f.read().split("\n\n")
+        calories = []
+        for elf in elves:
+            cals = map(int, elf.split("\n"))
+            calories.append(sum(cals))
+        print(f"Part 1: {max(calories)}")
+        print(f"Part 2: {sum(sorted(calories, reverse=True)[:3:1])}")
 
 
 if __name__ == '__main__':
