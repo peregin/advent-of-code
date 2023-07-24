66e04400c5260bb3acb85981e1a0f139f142522d
levi <velocorner.com@gmail.com>
2023-07-24 21:48:28 +0200

    lt

---
+++ src/main/scala/leetcode/easy/SingleNumber.scala
@@ -0,0 +1,7 @@
+package leetcode.easy
+
+object SingleNumber extends App:
+
+  def singleNumber(nums: Array[Int]): Int = nums.groupBy(identity).find(_._2.length == 1).get._1
+
+  println(singleNumber(Array(4,1,2,1,2)))
