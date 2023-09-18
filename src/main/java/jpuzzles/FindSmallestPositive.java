6558614b6dc817126bdac7f73ffcd9c96fdd3da8
levi <velocorner.com@gmail.com>
2023-09-18 07:49:00 +0200

    cyclic sort

--- src/main/java/jpuzzles/FindSmallestPositive.java
+++ src/main/java/jpuzzles/FindSmallestPositive.java
@@ -8,6 +8,20 @@ import java.util.stream.IntStream;
 public class FindSmallestPositive {
 
     public int firstMissingPositive(int[] nums) {
+        // cyclic sort on nums
+        var n = nums.length;
+        for (int i = 0; i < n; i++) {
+            var v = nums[i];
+            if (v >= 0 && v < n) {
+                var w = nums[v];
+                if (w >= 0 && w < n && v != w)
+                nums[i] = w;
+                nums[v] = v;
+            }
+        }
+        System.out.println(IntStream.of(nums).boxed().toList());
+
+
         var uniq = IntStream.of(nums).filter(i -> i > 0 && i <= nums.length).boxed().collect(Collectors.toSet());
         var c = 1;
         while (!uniq.isEmpty()) {
