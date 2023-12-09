b232590786ee32b37b2747565e651dc986d333d1
levi <velocorner.com@gmail.com>
2023-12-09 13:50:39 +0100

    day 7 - read the exercise 🙈

--- src/main/scala/aoc/aoc2023/Day7.scala
+++ src/main/scala/aoc/aoc2023/Day7.scala
@@ -4,7 +4,7 @@ import aoc.Aoc
 
 import scala.collection.parallel.CollectionConverters.*
 
-object Day7 extends aoc.Aoc("aoc2023/input7_1.txt", identity):
+object Day7 extends aoc.Aoc("aoc2023/input7.txt", identity):
 
   enum HandType(val rank: Int) extends Ordered[HandType]:
     case Five      extends HandType(6)
@@ -31,25 +31,26 @@ object Day7 extends aoc.Aoc("aoc2023/input7_1.txt", identity):
 
   def detectWithJokers(card: String): HandType =
     if (card.count(_ == 'J') > 0) card.replace("J", "").groupMapReduce(identity)(_ => 1)(_ + _).values.toList.sorted match {
-      case Nil              => HandType.Five      // all jokers
-      case List(1, 1, 1, 1) => HandType.OnePair   // 1 joker, all different
-      case List(1, 1, 2)    => HandType.Three     // 1 joker, 1 pair
-      case List(2, 2)       => HandType.FullHouse // 1 joker, 2 pairs
-      case List(1, 3)       => HandType.Four      // 1 joker, 3 same
-      case List(4)          => HandType.Five      // 1 joker 4 same
-      case other            => sys.error(s"unexpected $other")
+      case Nil                 => HandType.Five      // all jokers
+      case List(1, 1, 1, 1)    => HandType.OnePair   // 1 joker, all different
+      case List(1, 1, 1 | 2)   => HandType.Three     // x joker, 1 pair
+      case List(2, 2)          => HandType.FullHouse // 1 joker, 2 pairs
+      case List(1, 1 | 2 | 3)  => HandType.Four      // x joker, 3 same
+      case List(1 | 2 | 3 | 4) => HandType.Five      // x joker all the same
+      case other               => sys.error(s"unexpected $other")
     }
     else detectStraight(card) // no jokers
 
-  case class Hand(cards: String, bid: Int, handType: HandType) extends Ordered[Hand]:
+  case class Hand(cards: String, bid: Int, handType: HandType, joker: Boolean = false) extends Ordered[Hand]:
     // to sort the same type of hands
     lazy val numeric = cards.map {
-      case 'T' => "10"
-      case 'J' => "11"
-      case 'Q' => "12"
-      case 'K' => "13"
-      case 'A' => "14"
-      case d   => s"0$d"
+      case 'T'           => "10"
+      case 'J' if !joker => "11"
+      case 'J' if joker  => "00"
+      case 'Q'           => "12"
+      case 'K'           => "13"
+      case 'A'           => "14"
+      case d             => s"0$d"
     }
 
     override def compare(that: Hand): Int =
@@ -58,13 +59,7 @@ object Day7 extends aoc.Aoc("aoc2023/input7_1.txt", identity):
         case r => r
       }
 
-    def strongestWithJoker(): Hand = {
-      val labels = cards.toSet
-      if (labels.contains('J')) {
-        val max = labels.map(c => cards.replace('J', c)).map(detectStraight).max
-        this.copy(handType = max)
-      } else this
-    }
+    def strongestWithJoker(): Hand = this.copy(handType = detectWithJokers(cards), joker = true)
 
   def score(on: List[Hand]): Int = on.zipWithIndex.map((h, ix) => h.bid * (ix + 1)).sum
 
