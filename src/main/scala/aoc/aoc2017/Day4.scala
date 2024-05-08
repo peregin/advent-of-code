378886fa798071afdd3947a5c401678af9efd607
levi <velocorner.com@gmail.com>
2024-05-08 19:50:10 +0200

    easy to translate in another language

---
+++ src/main/scala/aoc/aoc2017/Day4.scala
@@ -0,0 +1,14 @@
+package aoc.aoc2017
+
+object Day4 extends aoc.Aoc("aoc2017/input4.txt", identity) {
+
+  val res1 = input.map{line =>
+    line.split(' ').groupMapReduce(identity)(_ => 1)(_ + _).values.forall(_ == 1)
+  }.count(_ == true)
+  println(s"res1=$res1")
+
+  val res2 = input.map { line =>
+    line.split(' ').map(_.sorted).groupMapReduce(identity)(_ => 1)(_ + _).values.forall(_ == 1)
+  }.count(_ == true)
+  println(s"res2=$res2")
+}
