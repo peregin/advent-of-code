ef4755a8879a4a2a49fd04a2513c09c6b3fb9df6
levi <velocorner.com@gmail.com>
2024-05-14 23:37:53 +0200

    puzzle in rust

--- src/main/rust/aoc2017/day1.rs
+++ src/main/rust/aoc2017/day1.rs
@@ -1,7 +1,14 @@
 
 fn main() {
     let input = include_str!("../../resources/aoc2017/input1.txt");
-    let lines: Vec<&str> = input.lines().collect();
-    //lines.map
-    println!("input is: {:?}", lines);
+    //println!("input is: {:?}", input);
+    let part1 = input.chars().zip(input.chars().cycle().skip(1))
+        .filter(|(a, b)| a == b)
+        .map(|(a, _)| a.to_digit(10).unwrap());
+    println!("part1 is: {:?}", part1.sum::<u32>());
+
+    let part2 = input.chars().zip(input.chars().cycle().skip(input.len()/2))
+        .filter(|(a, b)| a == b)
+        .map(|(a, _)| a.to_digit(10).unwrap());
+    println!("part2 is: {:?}", part2.sum::<u32>());
 }
