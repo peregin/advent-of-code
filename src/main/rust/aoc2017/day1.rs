ce656651de6d7c14743dc829b5eff8c1a108cde7
levi <velocorner.com@gmail.com>
2024-05-18 10:55:53 +0200

    add tests

--- src/main/rust/aoc2017/day1.rs
+++ src/main/rust/aoc2017/day1.rs
@@ -1,14 +1,39 @@
 
 fn main() {
     let input = include_str!("../../resources/aoc2017/input1.txt");
-    //println!("input is: {:?}", input);
+    println!("part1 is: {:?}", part1(input));
+    println!("part2 is: {:?}", part2(input));
+}
+
+fn part1(input: &str) -> u32 {
     let part1 = input.chars().zip(input.chars().cycle().skip(1))
         .filter(|(a, b)| a == b)
         .map(|(a, _)| a.to_digit(10).unwrap());
-    println!("part1 is: {:?}", part1.sum::<u32>());
+    part1.sum::<u32>()
+}
 
+fn part2(input: &str) -> u32 {
     let part2 = input.chars().zip(input.chars().cycle().skip(input.len()/2))
         .filter(|(a, b)| a == b)
         .map(|(a, _)| a.to_digit(10).unwrap());
-    println!("part2 is: {:?}", part2.sum::<u32>());
+    part2.sum::<u32>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static INPUT: &str = include_str!("../../resources/aoc2017/input1.txt");
+
+    #[test]
+    fn solve_part1() {
+        assert_eq!(part1("1122"), 3);
+        assert_eq!(part1(INPUT), 1136);
+    }
+
+    #[test]
+    fn solve_part2() {
+        assert_eq!(part2("1122"), 0);
+        assert_eq!(part2(INPUT), 1092);
+    }
 }
