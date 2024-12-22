c071cd4d446aff7fe1d78f82889b6d4c37e145f9
levi <velocorner.com@gmail.com>
2024-12-22 13:25:19 +0100

    day 22 - part1

---
+++ src/main/scala/aoc/aoc2024/Day22.scala
@@ -0,0 +1,14 @@
+package aoc.aoc2024
+
+import aoc.Aoc
+
+object Day22 extends Aoc("aoc2024/input22.txt", _.toLong):
+
+  private val mod = 16777216L
+  def s1(secret: Long): Long = ((secret * 64) ^ secret) % mod
+  def s2(secret: Long): Long = ((secret / 32) ^ secret) % mod
+  def s3(secret: Long): Long = ((secret * 2048) ^ secret) % mod
+  def calc(secret: Long): Long = s3(s2(s1(secret)))
+
+  val part1 = input.map(n => (1 to 2000).foldLeft(n)((accu, i) => calc(accu))).sum
+  println(s"part1=$part1")
